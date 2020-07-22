86984e0084a0b080a21a427b86e4559914e2d9eb
Niju <wanjofu26@live.com>
2020-07-22 19:31:42 +0300

    initial commit
    first transition exercise involving a full page

---
+++ rtransitions00/main.js
@@ -0,0 +1,15 @@
+changeBackground = () => {
+    let el = document.querySelector("div.full-page");
+
+    if(el) {
+        el.className = "full-pages";
+    }
+    else {
+        el = document.querySelector("div.full-pages");
+        el.className = "full-page";
+    }
+
+    return el;
+}
+
+var intervalID = window.setInterval(changeBackground, 7000);
